6d84ec4b252a79d2015097ae64ce208404f30ef1
guido jacofsky <166538636+guidojacoo@users.noreply.github.com>
2025-05-13 12:02:15 -0300

    terminado

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject cuboPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(cuboPrefab);
+    }
+
+    // Update is called once per frame
+    public void boton() { 
+                Instantiate(cuboPrefab);
+}
+}
